927d135f8d336eecabbebbbc825c4bc8b5e105dd
rin-lux <peanutbutterisbeast@gmail.com>
2025-05-03 23:21:05 -0400

    properly import log

--- src/holo/util/saveLoad.py
+++ src/holo/util/saveLoad.py
@@ -1,33 +1,43 @@
 import json
 from dataclasses import asdict
 from dataclasses import dataclass
-from pathlib import Path
+
+# from pathlib import Path
 from typing import Any
 
-import numpy.typing as npt
+from holo.util.log import logger
+
+# import numpy.typing as npt
 
 
 @dataclass
 class plotPred:
     """Class for storing plotting information."""
 
-    y_test_pred: npt.NDArray[Any]
-    y_test: npt.NDArray[Any]
-    y_train_pred: npt.NDArray[Any]
-    y_train: npt.NDArray[Any]
-    yerr_train: npt.NDArray[Any]
-    yerr_test: npt.NDArray[Any]
+    # y_test_pred: npt.NDArray[Any]
+    # y_test: npt.NDArray[Any]
+    # y_train_pred: npt.NDArray[Any]
+    # y_train: npt.NDArray[Any]
+    # yerr_train: npt.NDArray[Any]
+    # yerr_test: npt.NDArray[Any]
+    y_test_pred: list[Any]
+    y_test: list[Any]
+    y_train_pred: list[Any]
+    y_train: list[Any]
+    yerr_train: list[Any]
+    yerr_test: list[Any]
     title: str
-    fname: Path
+    fname: str
     save_fig: bool = True
 
 
 def load_obj() -> list[plotPred]:
-    """Load object of class plotPred."""
+    """Load saved json containing object of class plotPred."""
     try:
-        with open("cats.json") as fd:
+        with open("train_data.json") as fd:
             return [plotPred(**x) for x in json.load(fd)]
     except FileNotFoundError:
+        logger.exception(FileNotFoundError)
         return []
 
 
